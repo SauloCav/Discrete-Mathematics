0ed7ea8bcc0a6a89a9fe0358daae69a560182510
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-02-09 13:12:44 -0300

    Update Hanoi Tower.cpp

--- Tower of Hanoi/Hanoi Tower.cpp
+++ Tower of Hanoi/Hanoi Tower.cpp
@@ -6,9 +6,7 @@
 #include<windows.h>
 
 void to(int qd, char i, char f, char ar);
-
 void re();
-
 void mov(int x);
 
 int main(){
@@ -19,86 +17,83 @@ int main(){
 	
 	do{
 	
-	printf("BEM VINDO A TORRE DE HANOI");
-	
-	printf("\n\n|1| Regras");
-	printf("\n|2| Calculo de quantidade de movimentos");
-	printf("\n|3| Instrucao dos movimentos");
-	printf("\n|0| Sair");
-	
-	printf("\n\nEscolha uma das opcoes: ");
-	scanf("%d", &op);
-	
-	if(op==0){
+		printf("BEM VINDO A TORRE DE HANOI");
+		printf("\n\n|1| Regras");
+		printf("\n|2| Calculo de quantidade de movimentos");
+		printf("\n|3| Instrucao dos movimentos");
+		printf("\n|0| Sair");
+		printf("\n\nEscolha uma das opcoes: ");
+		scanf("%d", &op);
+
+		if(op==0){
 			printf("\nTCHAU!");
 		}
-	
-	switch(op){
-		
-		case 0:{
-			break;
-		}
-		
-		case 1:{
-			
-			re();
-			
-			
-			printf("\n\ninsira 0 para voltar ao menu: ");
-			scanf("%d", &z);
-			if(z==0){
-				system("cls");
+
+		switch(op){
+
+			case 0:{
+				break;
 			}
-			
-			break;
-		}
-		
-		case 2:{
-			
-			printf("\n\nInsira a quantidade de discos: ");
-			scanf("%d", &qd);
-			
-			mov(qd);
-			
-			printf("\n\ninsira 0 para voltar ao menu: ");
-			scanf("%d", &z);
-			if(z==0){
-				system("cls");
+
+			case 1:{
+
+				re();
+				printf("\n\ninsira 0 para voltar ao menu: ");
+				scanf("%d", &z);
+				if(z==0){
+					system("cls");
+				}
+
+				break;
+				
 			}
-			
-			break;
-		}
+
+			case 2:{
+
+				printf("\n\nInsira a quantidade de discos: ");
+				scanf("%d", &qd);
+				
+				mov(qd);
+				
+				printf("\n\ninsira 0 para voltar ao menu: ");
+				scanf("%d", &z);
+				if(z==0){
+					system("cls");
+				}
+
+				break;
 		
-		case 3:{
-			
-			printf("\n\nInsira a quantidade de discos: ");
-			scanf("%d", &qd);
-	
-			printf("\n");
-	
-			to(qd, 'A', 'C', 'B');
-			
-			printf("\n\ninsira 0 para voltar ao menu: ");
-			scanf("%d", &z);
-			if(z==0){
-				system("cls");
 			}
-			
-			break;
+
+			case 3:{
+
+				printf("\n\nInsira a quantidade de discos: ");
+				scanf("%d", &qd);
+				printf("\n");
+
+				to(qd, 'A', 'C', 'B');
+
+				printf("\n\ninsira 0 para voltar ao menu: ");
+				scanf("%d", &z);
+				if(z==0){
+					system("cls");
+				}
+
+				break;
+				
+			}
+
+			default:{
+
+				printf("\nVOCE INSERIU UM NUMERO INVALIDO!");
+				Sleep(2000);
+				system("cls");
+
+				break;
+				
+			}
+
 		}
-		
-		default:{
-			
-		printf("\nVOCE INSERIU UM NUMERO INVALIDO!");
-		
-		Sleep(2000);
-		
-		system("cls");
-			
-			break;
-		}
-		
-	}
 	
 	}while(op!=0);
 	
@@ -112,9 +107,9 @@ void to(int qd, char i, char f, char ar){
 	}
 	
 	else{
-	to(qd-1, i, ar, f);
-	printf("\npasse o disco %d da torre (%c) para a torre (%c)", qd, i, f);
-	to(qd-1, ar, f, i);
+		to(qd-1, i, ar, f);
+		printf("\npasse o disco %d da torre (%c) para a torre (%c)", qd, i, f);
+		to(qd-1, ar, f, i);
 	}
 	
 }
@@ -130,13 +125,10 @@ void re(){
 }
 
 void mov(int x){
-	
+
 	int m;
-	
 	m=(pow(2,x))-1;
-	
 	printf("\nNumero de discos: %d", x);
 	printf("\nNumero de movimentos necessarios: %d", m);
 	
 }
-
